673602186efa60265f6819354a7fd7ce56967fbf
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2019-03-28 11:28:49 +0000

    modifica cattiva

--- chiara.R
+++ chiara.R
@@ -12,7 +12,7 @@ fb_data <- fb_data %>%
   mutate(reactions = as.numeric(reactions)) %>%
   filter(!is.na(reactions))
 
-
+# il mio commento e piu importante 
 p1 <- ggplot(data = fb_data, aes(x=type, y = clicks)) +
   geom_boxplot()
 
